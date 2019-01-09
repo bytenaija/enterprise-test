8fa9096bf05506585e0d9791e27088b918b3560d
Everistus Olumese <everistusolumese@gmail.com>
2019-01-09 06:44:18 +0100

    Initial setup of start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
-set -e
+
 
 echo "# Downloading the docker install script"
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+sudo apt-get purge docker lxc-docker docker-engine docker.io
+
+sudo apt-get install  curl  apt-transport-https ca-certificates software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add 
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
+
+echo "# verifying docker install status"
+sudo systemctl status docker
 
 echo "# Downloading the docker-compose"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
