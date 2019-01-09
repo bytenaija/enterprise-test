f7ecf137a946a46ea981ae32910eb52e8af59e4b
Everistus Olumese <everistusolumese@gmail.com>
2019-01-09 08:31:00 +0100

    Initial setup of start.sh

--- start.sh
+++ start.sh
@@ -1,43 +1,24 @@
 #!/bin/bash
-
-
 echo "# Downloading the docker install script"
-
 sudo apt -y remove docker docker-engine docker.io && \
-
 sudo apt -y update && \
-
 sudo apt -y install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common && \
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add && \
-
 sudo apt-key fingerprint 0EBFCD88 && \
-
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable" && \
-
 sudo apt -y update && \
-
 sudo apt -y install docker-ce && \
-
 sudo docker run hello-world && \
-
-
 echo "# Downloading the docker-compose" && \
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
-
 echo "# Apply executable permissions to the docker compose binary" && \
-
 sudo chmod +x /usr/local/bin/docker-compose && \
-
 sudo docker-compose --version && \
-
 sudo docker-compose --verbose up
-
-
