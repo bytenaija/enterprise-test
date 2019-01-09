dbb3e2d82ca97a4369e01c2e21d1267afdcd897e
Everistus Olumese <everistusolumese@gmail.com>
2019-01-09 08:09:48 +0100

    Initial setup of start.sh

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ require('dotenv').config();
 app.use(cors());
 app.use(morgan('dev'));
 
+app.get("/", (req, res)=>{
+    res.send('We are running')
+})
 let PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`server runNing on PORT ${PORT}`));
