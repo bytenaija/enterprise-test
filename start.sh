7b6493c3d84be25cf1f5524b713bede02cd5f1bc
Everistus Olumese <everistusolumese@gmail.com>
2019-01-09 06:58:27 +0100

    Initial setup of start.sh

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ sudo apt-get install \
     curl \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add 
 
 sudo apt-key fingerprint 0EBFCD88
 
